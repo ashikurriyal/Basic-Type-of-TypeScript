ac6b331f3ae667985c78a2e1357dee04d05664fa
ashikurriyal <riyal.ashikur@gmail.com>
2025-05-04 21:35:51 +0600

    understanding the basic types of typescript

---
+++ Module1/source/1.4.ts
@@ -0,0 +1,37 @@
+//Basic data types
+
+//string
+
+//implicit type define 
+let firstName = 'riyal'
+
+//explicit type define
+let secondName : string = 'rahman'
+let roll : number = 123;
+
+//boolean
+let isAdmin : boolean = true;
+
+//undefined
+let x : undefined = undefined;
+
+// null
+let y: null = null;
+
+//here the the d varible typescript compiler will assume it can be any type cause we are not define here
+let d;
+
+
+//non premitive data types
+
+//array
+
+let friends : string[] = ['hasan', 'ali'];
+
+let randomRoll : number[] = [1,3]
+
+//tuple --> array --> order --> type of values
+
+let coordinates : [number, number] = [1,5];
+
+let ageName : [number, string, boolean] = [25, 'riyal', true]
